81e2904b355ef227d99d372881d0ce6e5e457ee4
Jennah <contactjennah@gmail.com>
2018-07-05 21:32:44 +0100

    Refactor: Make form a functional component keeping its state in App maintains unidirectional flow of info as form state is needed in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,15 +6,45 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      stashPoints: []
+      stashPoints: [],
+      city: '',
+      hotel: false,
+      tourist_information_centre: false,
+      queryParams: {}
     };
   }
+
+  /**
+   * Form submission
+   * Makes the API call and updates App state with the response from the API call
+   */
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  /**
+   * Form input value changes
+   * Updates the fields in the form i.e. unchecked checkbox becomes checked on click
+   */
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [event.target.id]: value
+    });
+  };
 
   render() {
     return (
       <div>
         <h1>StashPoint Search</h1>
-        <Form />
+        <Form
+          values={this.state}
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
       </div>
     );
   }
--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -1,42 +1,37 @@
 import React from 'react';
 
-export default class Form extends React.Component {
-  constructor(props) {
-    super(props);
+const Form = props => {
+  const { city, hotel, tourist_information_centre } = props.values;
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <label htmlFor="city">City </label>
+      <input
+        id="city"
+        placeholder="Enter a city"
+        value={city}
+        type="text"
+        onChange={props.handleChange}
+        required
+      />
+      <label htmlFor="hotel">Hotels</label>
+      <input
+        id="hotel"
+        type="checkbox"
+        checked={hotel}
+        onChange={props.handleChange}
+      />
+      <label htmlFor="tourist_information_centre">
+        Tourist Information Centre
+      </label>
+      <input
+        id="tourist_information_centre"
+        type="checkbox"
+        checked={tourist_information_centre}
+        onChange={props.handleChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
 
-    this.state = {
-      city: ''
-    };
-  }
-
-  // Updates the state in the form which is used for the form input values
-  // Ensures that the user can see what they are typing
-  handleChange = event => {
-    const cityVal = event.target.value;
-    this.setState({
-      city: cityVal
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log(this.state.city);
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="city">City </label>
-        <input
-          id="city"
-          placeholder="Enter a city"
-          value={this.state.city}
-          type="text"
-          onChange={this.handleChange}
-          Required
-        />
-        <button type="submit">Search</button>
-      </form>
-    );
-  }
-}
+export default Form;
