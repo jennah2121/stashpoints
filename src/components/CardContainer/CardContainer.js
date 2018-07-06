128c1976f8d22759a857f36c9f535c7c813bacd0
Jennah <contactjennah@gmail.com>
2018-07-06 15:20:12 +0100

    Styled card container

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -4,6 +4,10 @@ import Card from '../Card/card.js';
 
 const StyledUl = styled.ul`
   list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  grid-gap: 2rem;
+  padding: 1rem 1.4rem;
 `;
 
 const CardContainer = ({ stashPoints }) => {
